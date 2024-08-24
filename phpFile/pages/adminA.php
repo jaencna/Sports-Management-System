e60e4c751d579d282ff9cb1c659b3399efd83783
unknown <michaeldavenfort@gmail.com>
2024-08-24 10:53:09 +0800

    homepage

---
+++ phpFile/pages/adminA.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Homepage</title>
+  </head>
+  <body style="background-color: black; height: 2419px; background-image: url('../../images/homepage/TERRAFORM.png');background-size: cover; background-position: center; height: 100vh; background-repeat: no-repeat;overflow-x: hidden;">
+    
+    
+    <div class = "row">
+        <div class = "col-sm-1" style="background-color: rgb(29, 29, 29); height: 70px; color: white;"></div>
+        <div class = "col-sm-11" style="background-color: rgb(29, 29, 29); height: 70px; color: white; align-content: center;"> <img src="../../images/homepage/LOGO.png" height="50"> </div>
+        
+    
+
+      <div class = "row">
+        <div class = "col-sm-5" style="height: 150px; color: white; font-size: 90px; padding-top: 100px; padding-left: 100px;"> <b> <p>Play.
+          Score.
+          Win.</p></b></div>
+        <div class = "col-sm-7" style="height: 150px;"> <img src="../../images/homepage/basketbolero.png" class="img-responsive;" style="max-width: 100%; height: auto;"></div>
+      </div>
+
+<!--About-->
+
+        <div style="background-image: url('../../images/homepage/BOTT_HOME.png');background-size: cover; margin-top: 420px;">
+          <div class="row" style="margin-top: 40px; padding-left: 50px;">
+            <div class="col-sm-5"><img src="../../images/homepage/LOGO_CIRCLE.png" class="img-responsive;" style="max-width: 100%; height: auto;"></div>
+            <div class="col-sm-7"><p style="font-size: 56pt;color: white; font-family: Arial, Helvetica, sans-serif; margin-top: 50px; padding-left: 100px;"> 
+              <b>About</b>
+              </p>
+              <p style="color: white; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; padding-left: 100px; font-size: 15pt; margin-right: 100px; text-align: justify; line-height: 1.2;">
+      This is the SportsManager Pro. Welcome! Our objective is to simplify and improve the efficiency of administering sports teams, events, and activities. <br><br>
+      Our platform gives clubs, educational institutions, sports leagues, and individual athletes the tools and resources they need to run their organizations
+      more efficiently. We believe that sports have the power to unite people and inspire greatness. Our goal is to help coaches and athletes by offering
+      a wide range of management tools. Whether you're in charge of a community event, a league, or a competitive team, our approach addresses your unique
+      challenges.<br><br>
+      
+      Our user-friendly platform facilitates clear communication, performance monitoring, and scheduling. Join us as we transform sports activity
+      management to improve efficiency and enjoyment for all.
+
+              </p>
+  
+            </div>
+    
+          </div>
+
+          <div class = "row">
+            <div class = "col-sm-6" style="height: 150px; padding-top: 10px; padding-left: 120px;"> 
+
+<!--Services-->
+
+              <b><p style="color: white; font-size: 56pt; ">Services</p></b>
+
+              <p style="color: white; font-size: 20px;  text-align: justify; line-height: 1.1;">
+                Our SportManager Pro offers comprehensive tools to streamline sports team operations and athlete
+                management. Key features includes</p>
+              
+<!--Scoring-->
+              <div class="row">
+
+                <div class="col-sm-5" style=" padding-top: 65px"><img src="../../images/homepage/scoring.png" class="img-responsive;" style="max-width: 100%; height: auto;"></div>
+
+                <div class="col-sm-7">
+                  <p style="color: white;font-family: Arial, Helvetica, sans-serif; font-size: 24pt; margin-top: 50px; padding-left: 50px;line-height: 1.1;"><b>Scoring: </b>  </p>
+                  <p style="color: white; font-size: 20px; text-align: justify; padding-left: 50px;line-height: 1.1;"> Track and analyze game scores in real-time, 
+                    providing detailed statistics and performance metrics for teams and individual athletes.</p>
+                </div>
+              </div>  
+              
+<!--Coach Management-->
+
+              <div class="row">
+                <div class="col-sm-5" style=" padding-top: 40px"><img src="../../images/homepage/coachmng.png" class="img-responsive;" style="max-width: 100%; height: auto;">
+                </div>
+
+                <div class="col-sm-7">
+                  <p style="color: white;font-family: Arial, Helvetica, sans-serif; font-size: 24pt; padding-top: 20px; padding-left: 50px;line-height: 1.1;"><b>Coach Management: </b>  </p>
+                  <p style="color: white; font-size: 20px; text-align: justify; padding-left: 50px;line-height: 1.1;"> Efficiently manage coaching staff, including scheduling, performance tracking,
+                     and communication tools to enhance team coordination and development.</p>
+                </div>
+              </div>
+
+<!--Team-->
+
+              <div class="row">
+                <div class="col-sm-5" style=" padding-top: 40px"><img src="../../images/homepage/team.png" class="img-responsive;" style="max-width: 100%; height: auto;">
+                </div>
+
+                <div class="col-sm-7">
+                  <p style="color: white;font-family: Arial, Helvetica, sans-serif; font-size: 24pt; padding-top: 20px; padding-left: 50px;line-height: 1.1;"><b>Team and Athlete Data:</b>  </p>
+                  <p style="color: white; font-size: 20px; text-align: justify; padding-left: 50px;line-height: 1.1;"> Maintain detailed profiles for teams and athletes, including personal information, performance stats,
+                     training schedules, and medical records, ensuring all necessary data is easily accessible.</p>
+                </div>
+
+
+              </div>
+
+            </div>  
+            
+            
+                
+                <div class = "col-sm-6" style="padding-left: 85px; padding-top: 250px;">
+                  <img src="../../images/homepage/bolz.png" class="img-responsive;" style="max-width: 100%; height: auto;"></div>
+                
+
+              </div>
+            
+              
+          </div>
+
+        </div>
+   
+        
+
+      
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+  </body>
+
+  <footer>
+    <p>SportManager Pro.</p>
+
+  </footer>
+</html>
